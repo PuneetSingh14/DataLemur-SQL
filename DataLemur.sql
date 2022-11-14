e37f979d76be383de6f177e0092e9dc7a0cf0222
Puneet Singh <113958494+PuneetSingh14@users.noreply.github.com>
2022-11-14 14:46:24 +0530

    Update DataLemur.sql

--- DataLemur.sql
+++ DataLemur.sql
@@ -29,9 +29,9 @@ group by customer_id;
 -------------------------------------------------------------------------------------------------------------------------
 # Problem 1: linkedin (Data Science Skills)
 
-#Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job.
-You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
-Write a query to list the candidates who possess all of the required skills for the job. Sort the the output by candidate ID in ascending order.
+# Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job.
+ You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+ Write a query to list the candidates who possess all of the required skills for the job. Sort the the output by candidate ID in ascending order.
 
 Assumption:
 
