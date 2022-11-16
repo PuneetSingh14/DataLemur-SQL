530e1be0412180c1955b766a01b168eb85407b5d
Puneet Singh <113958494+PuneetSingh14@users.noreply.github.com>
2022-11-16 16:21:29 +0530

    Update DataLemur_Medium.sql

--- DataLemur_Medium.sql
+++ DataLemur_Medium.sql
@@ -1,6 +1,21 @@
 -------------------------------------------DataLemur (Medium_level)------------------------------------------------------
 # Problem Medium Level : Sportify Top 5 Artit
 
+# Assume there are three Spotify tables containing information about the artists, songs, and music charts. 
+Write a query to determine the top 5 artists whose songs appear in the Top 10 of the global_song_rank table the highest number of times.
+From now on, we'll refer to this ranking number as "song appearances".
+
+Output the top 5 artist names in ascending order along with their song appearances ranking (not the number of song appearances,
+but the rank of who has the most appearances). The order of the rank should take precedence.
+
+For example, Ed Sheeran's songs appeared 5 times in Top 10 list of the global song rank table; this is the highest number of appearances, 
+so he is ranked 1st. Bad Bunny's songs appeared in the list 4, so he comes in at a close 2nd.
+
+Assumptions:
+
+If two artists' songs have the same number of appearances, the artists should have the same rank.
+The rank number should be continuous (1, 2, 2, 3, 4, 5) and not skipped (1, 2, 2, 4, 5).
+
 create table if not exists artists (
 artist_id	integer,
 artist_name	varchar(100));
@@ -43,17 +58,17 @@ song_id	integer,
 `rank`	integer );
 
 Insert into global_song_rank values
-(1,	45202,	2),
-(3,	45202,	2),
+(1, 45202,	2),
+(3, 45202,	2),
 (15, 45202,	6),
-(2,	55511,	2),
-(1,	19960,	3),
-(9,	19960,	15),
+(2, 55511,	2),
+(1, 19960,	3),
+(9, 19960,	15),
 (23, 12636,	9),
 (24, 12636,	7),
-(2,	12636,	23),
+(2, 12636,	23),
 (29, 12636,	7),
-(1,	69820,	1),
+(1, 69820,	1),
 (17, 44552,	8),
 (11, 44552,	16),
 (11, 11254,	5),
@@ -69,8 +84,8 @@ Insert into global_song_rank values
 (23, 89633,	10),
 (52, 33101,	7),
 (20, 55511,	10),
-(7,	22222,	8),
-(8,	44552,	1),
+(7, 22222,	8),
+(8, 44552,	1),
 (40,11254,	10),
 (50, 89633,	1);
 
@@ -96,6 +111,44 @@ select * from global_song_rank;
     limit 6;
 ---------------------------------------------------------------------------------------------------------------------------------
     
+ # Problem : Highest-Grossing Items (Amazon)
     
+ # This is the same question as problem #12 in the SQL Chapter of Ace the Data Science Interview!
+
+Assume you are given the table containing information on Amazon customers and their spending on products in various categories.
+
+Identify the top two highest-grossing products within each category in 2022. Output the category, product, and total spend.
+
+
+CREATE TABLE PRODUCT_SPEND 
+(CATEGORY VARCHAR(50),
+PRODUCT VARCHAR(50),
+USER_ID INT,
+SPEND DECIMAL(50,2));
+
+INSERT INTO PRODUCT_SPEND VALUES
+('APPLIANCE', 'WASHING MACHINE', 123 ,219.80),
+('ELECTRONICS','VACUUM', 178, 152.00 ),
+('ELECTRONICS','WIRELESS HEADSET', 156, 249.90	),
+('ELECTRONICS','VACUUM', 145, 189.00 ),
+('ELECTRONICS','COMPUTER MOUSE', 195, 45.00),
+('APPLIANCE', 'REFRIGERATOR', 165, 246.00 ),
+('APPLIANCE', 'REFRIGERATOR', 123, 299.99 ),
+('APPLIANCE', 'WASHING MACHINE', 123, 220.00 ),
+('ELECTRONICS', 'VACUUM', 156, 145.66 ),
+('ELECTRONICS', 'WIRELESS HEADSET', 145, 198.00);
+
+SELECT * FROM PRODUCT_SPEND;
+
+SELECT * FROM  
+(SELECT CATEGORY, PRODUCT, SPEND, RANK() OVER (PARTITION BY CATEGORY ORDER BY SPEND DESC ) AS TOP_SPEND
+ FROM PRODUCT_SPEND) AS SP
+WHERE TOP_SPEND = 1;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
     
  
